ba59ce117cd8aba0d02a63e83535c6740e5ce8b4
xwp <xwp_fullstack@163.com>
2018-11-04 15:01:15 +0800

    添加静态文件和生成数据库

--- Information/HomePage/homepage_views.py
+++ Information/HomePage/homepage_views.py
@@ -1,6 +1,12 @@
 from . import blueprint_objt
+from flask import render_template,current_app
 
 
 @blueprint_objt.route("/")
 def homepage():
-    return "HomePage for test"
+    return render_template("news/index.html")
+
+
+@blueprint_objt.route("/favicon.ico")
+def add_favi():
+    return current_app.send_static_file("news/favicon.ico")
--- Information/__init__.py
+++ Information/__init__.py
@@ -1,10 +1,18 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 import redis
 from flask import Flask
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-
 from config import config_dic
+
+logging.basicConfig(level=logging.DEBUG)
+file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024*1024*100, backupCount=10)
+formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+file_log_handler.setFormatter(formatter)
+logging.getLogger().addHandler(file_log_handler)
 
 db = SQLAlchemy()
 
